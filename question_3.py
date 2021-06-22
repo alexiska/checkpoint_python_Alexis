622e253255d0d31047514492bbea88347d4402bf
Alexiska <alexiska@kth.se>
2021-06-22 11:50:06 +0200

    adding the key "score"

--- question_3.py
+++ question_3.py
@@ -14,8 +14,9 @@ def get_points(emp):
             if val > 5:
                 points += 100 # I assume that they will only get 100p once they reach obove the criteria for sales
     print(f"Total points: {points}")
+    return points
         
 jordan_belfort = {"calls": 178, "meetings":17,  "sales":6}
 bill_bull = {"calls": 138, "meetings":41,  "sales":4}
-get_points(jordan_belfort)
-get_points(bill_bull)
+jordan_belfort['score'] = get_points(jordan_belfort)
+bill_bull['score'] = get_points(bill_bull)
